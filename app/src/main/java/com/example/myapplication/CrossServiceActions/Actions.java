72e922d0f26d1c8a5c359f9938ed594a2bd1c4b2
Hieu H Vu <hieuvu927@gmail.com>
2022-01-14 15:17:24 -0600

    Ver 8.4.3
    Some comments added

--- app/src/main/java/com/example/myapplication/CrossServiceActions/Actions.java
+++ app/src/main/java/com/example/myapplication/CrossServiceActions/Actions.java
@@ -3,12 +3,23 @@ package com.example.myapplication.CrossServiceActions;
 /**
  * The message services and controller send each other.
  * Exception handling and result formatting are delegated to this hierarchy tree in the subclasses.
+ * This set up allows service users to treat all services' messages in the same way. A service user
+ * could be another service.
+ * Actions will be programmed as the subclass is created.
+ * Each message is independent and eventhough all messages inherit this class, its
+ * content is private.
  */
 public abstract class Actions {
-    String result;
+    private String result;
     protected MessageEffect postActionEffect;
 
-    protected void setResult(MessageEffect messageEffect){
+    /**
+     * Programs what the message will do. Functional interface is used to restrict
+     * overriding this method in order to prevent subclasses from loosening access to the message's
+     * content and to provide greater flexibility in input variables types.
+     * @param messageEffect effect to be provided
+     */
+    final void setResult(MessageEffect messageEffect){
        result = messageEffect.send();
     }
 
